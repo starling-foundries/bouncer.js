6e088834d926558aa3a9cab2da3eb619ed36e26e
Cameron Sajedi <cameronsajedi@gmail.com>
2019-08-15 00:44:51 +0100

    contract utilities

---
+++ src/internal/contracts/hello.js
@@ -0,0 +1,78 @@
+
+const code = `scilla_version 0
+
+(* HelloWorld contract *)
+
+import ListUtils
+
+(***************************************************)
+(*               Associated library                *)
+(***************************************************)
+library HelloWorld
+
+let one_msg = 
+  fun (msg : Message) => 
+  let nil_msg = Nil {Message} in
+  Cons {Message} msg nil_msg
+  
+let not_owner_code = Int32 1
+let set_hello_code = Int32 2
+
+(***************************************************)
+(*             The contract definition             *)
+(***************************************************)
+
+contract HelloWorld
+(owner: ByStr20)
+
+field welcome_msg : String = ""
+
+transition setHello (msg : String)
+  is_owner = builtin eq owner _sender;
+  match is_owner with
+  | False =>
+    e = {_eventname : "setHello()"; code : not_owner_code};
+    event e
+  | True =>
+    welcome_msg := msg;
+    e = {_eventname : "setHello()"; code : set_hello_code};
+    event e
+  end
+end
+
+
+transition getHello ()
+    r <- welcome_msg;
+    e = {_eventname: "getHello()"; msg: r};
+    event e
+end
+
+transition multipleMsgs()
+  msg1 = {_tag : ""; _recipient : _sender; _amount : Uint128 0};
+  msg2 = {_tag : ""; _recipient : _sender; _amount : Uint128 0};
+  msgs1 = one_msg msg1;
+  msgs2 = Cons {Message} msg2 msgs1;
+  send msgs2
+end
+
+transition contrAddr()
+  msg1 = {_eventname : "ContractAddress"; addr : _this_address };
+  event msg1
+end`;
+
+const init = [
+    // this parameter is mandatory for all init arrays
+    {
+      vname: "_scilla_version",
+      type: "Uint32",
+      value: "0"
+    },
+    {
+      vname: "owner",
+      type: "ByStr20",
+      // NOTE: all byte strings passed to Scilla contracts _must_ be
+      // prefixed with 0x. Failure to do so will result in the network
+      // rejecting the transaction while consuming gas!
+      value: "zil1ntmmkpzjecdjjarvnqwturkg2694u969f6l0ew"
+    }
+  ];
